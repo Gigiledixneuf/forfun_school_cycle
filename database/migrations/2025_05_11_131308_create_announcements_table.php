2cc6760306666078f5bdbe66c2b8a78dedbbfe26
Guerchom Kubaluka <guerchomkubaluka@gmail.com>
2025-05-11 20:28:13 +0100

    feat : CRUD announcement et upload photo

--- database/migrations/2025_05_11_131308_create_announcements_table.php
+++ database/migrations/2025_05_11_131308_create_announcements_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
     {
         Schema::create('announcements', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(Tutor::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete();
             $table->foreignIdFor(Category::class)->constrained()->cascadeOnDelete();
             $table->string('title');
             $table->text('description');
